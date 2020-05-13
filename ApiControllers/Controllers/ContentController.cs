53f77e3588f87b8180990655a6596c17a5a5aef6
unknown <i.p.m.a.n.03.31.87@gmail.com>
2020-05-13 15:03:34 -0500

    first commit

---
+++ ApiControllers/Controllers/ContentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ApiControllers.Models;
+
+
+namespace ApiControllers.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContentController : Controller
+    {
+        [HttpGet("string")]
+        public string GetString() => "This is string response";
+
+        [HttpGet("object/{format?}")]
+        [FormatFilter]//overrides any formats specified by the Accept header in a request (gives developer more control)
+        //[Produces("application/json", "application/xml")] //specify format in response - overrides content negotiation process
+        public Reservation GetObject() => new Reservation
+        {
+            ReservationId = 100,
+            ClientName = "Jimmy",
+            Location = "Board room"
+        };
+
+        [HttpPost]
+        [Consumes("application/json")]
+        public Reservation ReceiveJson([FromBody] Reservation reservation)
+        {
+            reservation.ClientName = "Json";
+            return reservation;
+        }
+
+        [HttpPost]
+        [Consumes("application/xml")]
+        public Reservation ReceiveXml([FromBody] Reservation reservation)
+        {
+            reservation.ClientName = "Xml";
+            return reservation;
+        }
+    }
+}
---
+++ ApiControllers/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using ApiControllers.Models;
+using Microsoft.Net.Http.Headers;
+
+namespace ApiControllers
+{
+    public class Startup
+    {        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IRepository, MemoryRepository>();
+
+            services.AddMvc()
+                //gives mvc ability to send xml back in its response
+                .AddXmlDataContractSerializerFormatters()
+
+                //adding shorthand mapping for xml (json mapping is there by default)
+                .AddMvcOptions(opts =>
+                {
+                    opts.FormatterMappings.SetMediaTypeMappingForFormat("xml",
+                        new MediaTypeHeaderValue("application/xml"));
+                    opts.RespectBrowserAcceptHeader = true;
+                    
+                    //sends 406-Not Acceptable to client if a formatter not found
+                    opts.ReturnHttpNotAcceptable = true;
+                });
+        }
+                
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
